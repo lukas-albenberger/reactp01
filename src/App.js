84d4baf5026351cf253edb846393e7cc66c275a8
Lukas Albenberger <54547833+Baradil@users.noreply.github.com>
2020-07-13 15:24:44 +0200

    P01 - Implement Weather API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import keys from "./keys";
+
+const api = {
+  key: keys.API_KEY,
+  base: keys.BASE_URL,
+};
 
 function App() {
+  const dateBuild = (d) => {
+    let date = String(new window.Date());
+    date = date.slice(3, 15);
+    return date;
+  };
+
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+  const search = (e) => {
+    if (e.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then((res) => res.json())
+      .then((result) => {
+        setQuery("");
+        setWeather(result);
+        console.log(result);
+      });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div 
+      className = {
+        typeof setWeather.main != "undefined"
+        ? setWeather.main.temp > 18 ?
+        "App hot" : "App cold" : "App"
+      }
+    >
+      <main>
+        <div className ="search-container">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="search-bar"
+            onChange={(e) => setQuery(e.target.value)} 
+            value={query}
+            onKeyPress={search}
+            />
+        </div>
+        {typeof setWeather.main != "undefined" ? (
+          <div>
+            <div className = "location-container">
+              <div className = "location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date"> {dateBuild(new Date())}</div>
+              </div>
+              <div className="weather-container">
+                <div className="temperature">
+                  {Math.round(weather.main.temp)}°C
+                </div>
+                <div className="weather">{weather.weather[0].main}</div>
+              </div>
+          </div>
+        ) : ("")} 
+      </main>
     </div>
   );
 }
 
-export default App;
+        export default App;
