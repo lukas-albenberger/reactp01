414cfaec266743964b33417555d11f5a82f42a6d
Lukas Albenberger <54547833+Baradil@users.noreply.github.com>
2020-07-15 14:06:51 +0200

    changed app to location based weather

--- src/App.js
+++ src/App.js
@@ -1,70 +1,136 @@
-import React, {useState} from 'react';
+import React from 'react';
+import './App.css'
 import keys from "./keys";
+
+import { WeatherData } from './components/WeatherData';
+import { StatusData } from './components/StatusData';
 
 const api = {
   key: keys.API_KEY,
   base: keys.BASE_URL,
 };
 
-function App() {
-  const dateBuild = (d) => {
-    let date = String(new window.Date());
-    date = date.slice(3, 15);
-    return date;
-  };
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: 'init',
+      isLoaded: false,
+      weatherData: null
+    }
+  }
 
-  const [query, setQuery] = useState("");
-  const [weather, setWeather] = useState({});
-  const search = (e) => {
-    if (e.key === "Enter") {
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-      .then((res) => res.json())
-      .then((result) => {
-        setQuery("");
-        setWeather(result);
-        console.log(result);
+  abortController = new AbortController();
+  controllerSignal = this.abortController.signal;
+
+  weatherInit = () => {
+
+    const success = (position) => {
+      this.setState({status: 'fetching'});
+      localStorage.setItem('location-allowed', true);
+      this.getWeatherData(position.coords.latitude, position.coords.longitude);
+    }
+  
+    const error = () => {
+      this.setState({status: 'unable'});
+      localStorage.removeItem('location-allowed');
+      alert('Unable to retrieve location.');
+    }
+  
+    if (navigator.geolocation) {
+      this.setState({status: 'fetching'});
+      navigator.geolocation.getCurrentPosition(success, error);
+    } else {
+      this.setState({status: 'unsupported'});
+      alert('Your browser does not support location tracking, or permission is denied.');
+    }
+  }
+
+getWeatherData = (lat, lon) => {
+  const weatherApi = `${api.base}weather?lat=${lat}&lon=${lon}&units=metric&appid=${api.key}`;
+  
+  fetch(weatherApi, { signal: this.controllerSignal })
+  .then(res => res.json())
+  .then(
+    (result) => {
+
+      console.log(result);
+
+      const { name } = result;
+      const { country } = result.sys;
+      const { temp, temp_min, temp_max, feels_like, humidity } = result.main;
+      const { description, icon } = result.weather[0];
+      const { speed, deg } = result.wind;
+
+      this.setState({
+        status: 'success',
+        isLoaded: true,
+        weatherData: {
+          name,
+          country,
+          description,
+          icon,
+          temp: temp.toFixed(1),
+          feels_like: feels_like.toFixed(1),
+          temp_min: temp_min.toFixed(1),
+          temp_max: temp_max.toFixed(1),
+          speed,
+          deg,
+          humidity
+        }
+      });
+    },
+    (error) => {
+      this.setState({
+        isLoaded: true,
+        error
       });
     }
-  };
+  );
+}
 
+componentDidMount() {
+  if(localStorage.getItem('location-allowed')) {
+   this.weatherInit();
+  } else {
+    return;
+  }
+}
+
+componentWillUnmount() {
+  this.abortController.abort();
+}
+
+returnActiveView = (status) => {
+  switch(status) {
+    case 'init':
+      return(
+        <button className='btn-main' onClick={this.onClick}
+        >
+          Get My Location
+        </button>
+      );
+      case 'success':
+      return <WeatherData data={this.state.weatherData} />;
+      default:
+        return <StatusData status={status} />;
+  }
+}
+
+onClick = () => {
+  this.weatherInit();
+}
+
+render() {
   return (
-    <div 
-      className = {
-        typeof setWeather.main != "undefined"
-        ? setWeather.main.temp > 18 ?
-        "App hot" : "App cold" : "App"
-      }
-    >
-      <main>
-        <div className ="search-container">
-          <input
-            type="text"
-            placeholder="Search..."
-            className="search-bar"
-            onChange={(e) => setQuery(e.target.value)} 
-            value={query}
-            onKeyPress={search}
-            />
-        </div>
-        {typeof setWeather.main != "undefined" ? (
-          <div>
-            <div className = "location-container">
-              <div className = "location">
-                {weather.name}, {weather.sys.country}
-              </div>
-              <div className="date"> {dateBuild(new Date())}</div>
-              </div>
-              <div className="weather-container">
-                <div className="temperature">
-                  {Math.round(weather.main.temp)}°C
-                </div>
-                <div className="weather">{weather.weather[0].main}</div>
-              </div>
-          </div>
-        ) : ("")} 
-      </main>
+    <div className='App'>
+      <div className='container'>
+        {this.returnActiveView(this.state.status)}
+      </div>
     </div>
   );
 }
 
-        export default App;
+}
+
+export default App;
--- src/keys.js
+++ src/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    API_KEY: "<b03e7b7f40cc016bf2b8963f36e70ad9>",
-    BASE_URL: "api.openweathermap.org/data/2.5/",
+    API_KEY: "b03e7b7f40cc016bf2b8963f36e70ad9",
+    BASE_URL: "http://api.openweathermap.org/data/2.5/",
 };
